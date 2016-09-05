0fa78465cdb5e48a635129febde9852704ff2c6d
drew <adrmcgarrity@gmail.com>
2016-09-05 15:49:55 -0400

    blog engine

---
+++ public/tags/my-blog.tag
@@ -0,0 +1,14 @@
+<my-blog>
+	<div each={ posts }>
+		<article>
+		<h3>{ attributes.title }</h3>
+		{ body }</article>
+	</div>
+
+	this.on('mount', function() {
+
+		this.posts = opts.posts;
+		this.update();
+	});
+
+</my-blog>
--- server.js
+++ server.js
@@ -1,13 +1,35 @@
-var http = require('http');
-var connect = require('connect');
-var serveStatic = require('serve-static');
-var githooked = require('githooked');
+const http = require('http'),
+      connect = require('connect'),
+      serveStatic = require('serve-static'),
+      githooked = require('githooked'),
+      markdown = require('markdown-parse'),
+      fs = require('fs'),
+      send = require('connect-send-json');
 
 console.log('\n\n--- Node Version: ' + process.version + ' ---');
 
 // Set up Connect routing
 var app = connect()
     .use(serveStatic(__dirname + '/public'))
+    .use(send.json())
+    .use('/posts', function(req, res, next) {
+
+        var results = [];
+
+        var fileNames = fs.readdirSync(__dirname + '/public/posts/');
+
+        fileNames.forEach(function(fileName) {
+
+            var content = fs.readFileSync(__dirname + '/public/posts/' + fileName, 'utf-8');
+
+            markdown(content, function(err, result) {
+
+                results.push(result);
+            });
+        });
+
+        res.send(results);
+    })
     .use(function(req, res) {
         console.log('Could not find handler for: ' + req.url);
         res.end('Could not find handler for: ' + req.url);
